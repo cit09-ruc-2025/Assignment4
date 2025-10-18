cd0bfd5e1b3852befee6e9ee0f0c10aa09bad293
Bressendorff <frederik@bressendorff.eu>
2025-10-19 01:04:52 +0200

    Added CategoriesController, using a DTO with only the necessary information

---
+++ DataServiceLayer/DTO/CategoryDTO.cs
@@ -0,0 +1,30 @@
+﻿using DataServiceLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataServiceLayer.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public static CategoryDTO ConvertToDTO(Category category)
+        {
+            return new CategoryDTO()
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description
+            };
+        }
+
+        public static List<CategoryDTO> ConvertToDTO(IEnumerable<Category> categories)
+        {
+            return categories.Select(ConvertToDTO).ToList();
+        }
+    }
+}
---
+++ WebServiceLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using DataServiceLayer;
+using DataServiceLayer.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+
+        public CategoriesController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<List<CategoryDTO>> GetCategories()
+        {
+            var categories = _dataService.GetCategories();
+            if (!categories.Any()) return NoContent();
+
+            return Ok(CategoryDTO.ConvertToDTO(categories));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CategoryDTO> Get(int id)
+        {
+            var category = _dataService.GetCategory(id);
+            if (category is null) return NotFound();
+
+            return Ok(CategoryDTO.ConvertToDTO(category));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<CategoryDTO> Post([FromBody] CategoryDTO category)
+        {
+            var createdCategory = _dataService.CreateCategory(category.Name, category.Description);
+
+            return Created("/" + createdCategory.Id, CategoryDTO.ConvertToDTO(createdCategory));
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<bool> Put(int id, [FromBody] CategoryDTO category)
+        {
+            var isCategoryUpdated = _dataService.UpdateCategory(id, category.Name, category.Description);
+            return isCategoryUpdated ? Ok(isCategoryUpdated) : NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<bool> Delete(int id)
+        {
+            var isDeleted = _dataService.DeleteCategory(id);
+            return isDeleted ? Ok(isDeleted) : NotFound();
+        }
+    }
+}
