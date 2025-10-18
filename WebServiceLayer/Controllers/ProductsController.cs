7c6b0eaea3ae969e593a724e7750d2372427d9bb
dipika-baj <dipikabajracharya57@gmail.com>
2025-10-18 15:41:44 +0200

    get product with id

---
+++ WebServiceLayer/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceLayer.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDataService _dataService;
+
+        public ProductsController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _dataService.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("category/{id}")]
+        public IActionResult GetProductsByCategory(int id)
+        {
+            var products = _dataService.GetProductByCategory(id);
+            if (products.Count < 1)
+            {
+                return NotFound(Array.Empty<object>());
+            }
+            return Ok(products);
+        }
+
+    }
+}
--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -1,12 +1,17 @@
+using DataServiceLayer;
+
 namespace WebServiceLayer;
 
 public class Program
 {
+    public static DataService DataService { get; } = new DataService();
+
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddSingleton<IDataService, DataService>();
 
         builder.Services.AddControllers();
 
